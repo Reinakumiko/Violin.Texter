b21e1a01e34f3e49898e1185a320e6aae76426d3
_Kumiko <1164114736@qq.com>
2017-06-10 16:42:53 +0800

    添加部分安全检查代码

---
+++ Violin.Texter.Core/Checker/ArgumentCheck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Violin.Texter.Core.Checker
+{
+	public static class ArgumentCheck
+	{
+		public static void CheckNull(params object[] args)
+		{
+			var nullArgument = args.Where(arg => arg.IsNull()).Select(arg => arg);
+
+			foreach (var arg in nullArgument)
+			{
+				throw new ArgumentNullException();
+			}
+		}
+
+		public static bool IsNull<T>(this T obj)
+		{
+			return ReferenceEquals(obj, null);
+		}
+	}
+}
---
+++ Violin.Texter.Core/Checker/PathChecker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Violin.Texter.Core.Checker
+{
+	public static class PathChecker
+	{
+		public static void Check(this FileInfo fileInfo)
+		{
+			fileInfo.Directory.CheckDirectory();
+
+			if (!fileInfo.Exists)
+				fileInfo.Create().Dispose();
+		}
+
+		public static void Check(string path)
+		{
+			var fileInfo = new FileInfo(path);
+
+			fileInfo.Check();
+		}
+
+		public static void CheckDirectory(this DirectoryInfo dirInfo)
+		{
+			if (!dirInfo.Exists)
+				dirInfo.Create();
+		}
+
+		public static void CheckDirectory(string path)
+		{
+			var dirInfo = new DirectoryInfo(path);
+			dirInfo.CheckDirectory();
+		}
+	}
+}
