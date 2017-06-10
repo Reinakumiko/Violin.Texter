b28ad642409b192310df3d1ade15716ad87bed87
_Kumiko <1164114736@qq.com>
2017-06-10 16:42:23 +0800

    测试代码禁用警告

--- Violin.Texter.Console/Program.cs
+++ Violin.Texter.Console/Program.cs
@@ -10,7 +10,9 @@ namespace Violin.Texter.Console
 	{
 		static void Main(string[] args)
 		{
+#pragma warning disable CS0219 // 变量已被赋值，但从未使用过它的值
 			var str = "country_capitulated.0.d:0 \"[Root.GetName] has lost the war and has surrendered.There seems to be no way their military is able to continue the fight.\"";
+#pragma warning restore CS0219 // 变量已被赋值，但从未使用过它的值
 
 			//var ymlObject = new Deserializer().Deserialize<Dictionary<string, string>>(str);
 
