a7a16f64d688dad14f00bd418d7693f8f72f94bc
_Kumiko <1164114736@qq.com>
2017-06-12 00:17:56 +0800

    添加实现了属性更改提醒的列表

---
+++ Violin.Texter/Notify/NotifyList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Violin.Texter.Notify
+{
+	public class NotifyList<T> : List<T>, INotifyCollectionChanged, INotifyPropertyChanged, IList<T>, IList, IEnumerable, IEnumerable<T>, ICollection, ICollection<T>, IReadOnlyList<T>, IReadOnlyCollection<T>
+		where T : INotifyPropertyChanged
+	{
+		public event NotifyCollectionChangedEventHandler CollectionChanged;
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public NotifyList() : base () { }
+		public NotifyList(int capacity) : base(capacity) { }
+		public NotifyList(IEnumerable<T> list) : base(list) { }
+
+		public new void Add(T item)
+		{
+			base.Add(item);
+
+			item.PropertyChanged += OnCollectionPropertyChange;
+		}
+
+		private void OnCollectionPropertyChange(object sender, PropertyChangedEventArgs e)
+		{
+			CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset, sender));
+		}
+	}
+}
