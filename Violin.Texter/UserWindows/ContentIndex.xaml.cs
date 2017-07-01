f66e313131b17b084d2e58f9875c31aa65c22ff0
_Kumiko <1164114736@qq.com>
2017-07-01 18:04:56 +0800

    实现了查找窗体的查找功能

--- Violin.Texter/UserWindows/ContentIndex.xaml.cs
+++ Violin.Texter/UserWindows/ContentIndex.xaml.cs
@@ -1,19 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using MahApps.Metro.Controls;
-using Violin.Texter.Core.IndexEnumeration;
+using Violin.Texter.Core.ContentIndex;
+using Violin.Texter.Core.Enumeration;
 using Violin.Texter.Core.Progresses;
+using Violin.Texter.Core.Translations;
 
 namespace Violin.Texter.UserWindows
 {
@@ -24,6 +16,9 @@ namespace Violin.Texter.UserWindows
 	{
 		private static ContentIndex _instance;
 
+		/// <summary>
+		/// 获得窗体的实例
+		/// </summary>
 		public static ContentIndex Instance
 		{
 			get
@@ -32,6 +27,9 @@ namespace Violin.Texter.UserWindows
 			}
 		}
 
+		/// <summary>
+		/// 表示搜索时的查询范围
+		/// </summary>
 		public IndexZones IndexZone
 		{
 			get
@@ -40,6 +38,9 @@ namespace Violin.Texter.UserWindows
 			}
 		}
 
+		/// <summary>
+		/// 表示搜索时的查询方向
+		/// </summary>
 		public IndexDirection IndexDirection
 		{
 			get
@@ -48,35 +49,93 @@ namespace Violin.Texter.UserWindows
 			}
 		}
 
+		/// <summary>
+		/// 搜索时是否区分大小写
+		/// </summary>
+		public bool IsSensitiveCase
+		{
+			get
+			{
+				return _sensitiveCase.IsChecked ?? false;
+			}
+		}
+
+		/// <summary>
+		/// 搜索时是否使用正则表达式
+		/// </summary>
+		public bool IsUseRegex
+		{
+			get
+			{
+				return _useRegex.IsChecked ?? false;
+			}
+		}
+
+		/// <summary>
+		/// 表示要搜索的内容
+		/// </summary>
+		public string SearchContent
+		{
+			get
+			{
+				return _searchContent.Text;
+			}
+		}
+
+		/// <summary>
+		/// 表示上一次搜索时的内容
+		/// </summary>
+		public string LastSearchContent { get; set; }
+
+		/// <summary>
+		/// 表示上一次搜索到结果时的索引
+		/// </summary>
 		public int LastIndex { get; set; }
+
 		public EditProgress EditProgress { get; set; }
+		public TranslationEnumeration<Translation> _enumeration { get; set; }
 
 		private ContentIndex()
 		{
 			WindowStartupLocation = WindowStartupLocation.CenterOwner;
 			InitializeComponent();
-		}
 
-		public new void Show()
-		{
-			throw new NotSupportedException("该窗体不支持此方式展示");
-		}
-
-		public void Show(EditProgress progress)
-		{
-			EditProgress = progress;
-
-			base.Show();
+			SetDisplayMessage(string.Empty);
 		}
 
 		public int NextIndex()
 		{
-			return 0;
+			var options = new IndexOptions()
+			{
+				Direction = IndexDirection,
+				Zone = IndexZone,
+				IsUseRegex = IsUseRegex,
+				IsSensitiveCase = IsSensitiveCase
+			};
+
+			var result = _enumeration.GetNext(SearchContent, options);
+			return result.Key;
 		}
 
 		public void ResetIndex()
 		{
-			LastIndex = 0;
+			_enumeration.Reset(IndexDirection == IndexDirection.Next);
+		}
+
+		public void SetIndex(uint index)
+		{
+			_enumeration.SetIndex(index);
+		}
+
+		public void SetDisplayMessage(string message)
+		{
+			SetDisplayMessage(message, Brushes.Black);
+		}
+
+		public void SetDisplayMessage(string message, Brush color)
+		{
+			_resultText.Text = message;
+			_resultText.Foreground = color;
 		}
 
 		private IndexZones GetCurrentIndexZone()
@@ -111,6 +170,74 @@ namespace Violin.Texter.UserWindows
 
 			//将拥有者设置为空
 			this.Owner = null;
+		}
+
+		private void _getNext_Click(object sender, RoutedEventArgs e)
+		{
+			if (Owner == null)
+			{
+				SetDisplayMessage("需要查找的内容源为空。", Brushes.Red);
+				return;
+			}
+
+			if(string.IsNullOrWhiteSpace(SearchContent))
+			{
+				SetDisplayMessage("输入的查找内容为空。", Brushes.Red);
+				return;
+			}
+
+			if (_enumeration == null || _enumeration.List != EditProgress.Translations || _enumeration.List.Count != EditProgress.Translations.Count)
+				_enumeration = new TranslationEnumeration<Translation>(EditProgress.Translations);
+			
+			if (LastSearchContent != SearchContent)
+			{
+				ResetIndex();
+				LastIndex = -1;
+				LastSearchContent = SearchContent;
+			}
+
+			var nextIndex = NextIndex();
+
+			if (nextIndex == -1)
+			{
+				if (LastIndex == -1)
+					SetDisplayMessage("未找到匹配的文本项。");
+				else
+					SetDisplayMessage("搜索达到了尽头，下次搜索将会从头开始。", Brushes.Red);
+
+				ResetIndex();
+				return;
+			}
+
+			//重置消息(成功)
+			SetDisplayMessage("");
+
+			Owner.Invoke(() =>
+			{
+				LastIndex = nextIndex;
+				var keyList = (Owner as MainWindow).keyList;
+				keyList.SelectedIndex = LastIndex;
+
+				//将列表中的位置定位到已查找到项的位置
+				keyList.ScrollIntoView(keyList.SelectedItem);
+				
+				/* 这段感觉起来效果还不如 ScrollIntoView()
+				var border = VisualTreeHelper.GetChild(keyList, 0) as Border;
+
+				if (border != null)
+				{
+					var scrollViewer = border.Child as ScrollViewer;
+					if (scrollViewer != null)
+					{
+						scrollViewer.ScrollToVerticalOffset(nextIndex);
+					}
+				}
+				*/
+			});
+
+			//焦点设置回查找窗体
+			//感觉没必要啊
+			this.Focus();
 		}
 	}
 }
