d7ea0f454f30d3918cb98f8f0a9e1208271d2986
_Kumiko <1164114736@qq.com>
2017-07-01 18:01:27 +0800

    添加查找时使用的查找选项

---
+++ Violin.Texter.Core/ContentIndex/IndexOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Violin.Texter.Core.ContentIndex
+{
+	public struct IndexOptions
+	{
+		public IndexDirection Direction { get; set; }
+		public IndexZones Zone { get; set; }
+		public bool IsUseRegex { get; set; }
+		public bool IsSensitiveCase { get; set; }
+	}
+}
